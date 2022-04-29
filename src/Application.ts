d2d820574a3147e9e185c69683963b4d7f5d81a9
Stef Velzel <stefvelzel@hotmail.com>
2022-04-29 16:57:52 +0200

    Explicit app stopping

--- src/Application.ts
+++ src/Application.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import chalk from "chalk";
 
 export type Config = {
     port: number,
@@ -8,13 +9,28 @@ export type Config = {
 export class Application {
     private readonly express: express.Express;
 
+    // Helper to handle signals.
+    private registerEvents(): void {
+        process.on('SIGTERM', this.stop.bind(this));
+        process.on('SIGINT', this.stop.bind(this));
+        process.on('uncaughtException', (e) => console.error(e));
+        process.on('unhandledRejection', (r) => console.error(r));
+    }
+
     constructor(private readonly config: Config) {
+        this.registerEvents();
         this.express = express();
         this.express.disable('x-powered-by');
         this.express.use(express.json());
     }
 
-    async start(): Promise<void> {
+    start(): void {
+        console.log('Starting @' + chalk.underline(this.config.path));
         this.express.listen(this.config.port);
+    }
+
+    stop(): void {
+        console.log('Stopping @' + chalk.underline(this.config.path));
+        process.exit();
     }
 }
