d3acf8be4060ec1877eb6dfe9a9a34b4a3b3c501
Stef Velzel <stefvelzel@hotmail.com>
2022-05-03 14:27:16 +0200

    Basic request validation

--- src/http/JsonRpcProtocolVersion.js
+++ src/http/JsonRpcProtocolVersion.js
@@ -2,3 +2,5 @@
 export const JsonRpcProtocolVersion = {
     V2_0: "2.0"
 };
+
+export const JsonRpcProtocolVersionList = Object.values(JsonRpcProtocolVersion);
--- src/service/validateRequestBody.js
+++ src/service/validateRequestBody.js
@@ -1,4 +1,23 @@
+import { JsonRpcMethodList, JsonRpcProtocolVersionList } from "../http/index.js";
+
+function isObject(value) {
+    return typeof value === 'object' && !Array.isArray(value) && value !== null;
+}
+
+function isString(value) {
+    return typeof value === 'string' || value instanceof String;
+}
 
 export function validateRequestBody(body) {
-    throw new Error('nop dis wrong');
+    if (!JsonRpcProtocolVersionList.includes(body.jsonrpc))
+        throw new Error('Unsupported or missing JSON-RPC protocol version');
+
+    if (!isString(body.id))
+        throw new Error('Invalid or missing request identifier string');
+
+    if (!JsonRpcMethodList.includes(body.method))
+        throw new Error('Unsupported or missing JSON-RPC method');
+
+    if (!isObject(body.params))
+        throw new Error('Invalid or missing request parameters value');
 }
