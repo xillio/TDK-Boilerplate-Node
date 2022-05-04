3e6b52123bc3f971c7695c03f6ac493de6b7cbb9
Stef Velzel <stefvelzel@hotmail.com>
2022-05-04 14:15:49 +0200

    Started on method specific validation

--- src/jsonrpc/validate.js
+++ src/jsonrpc/validate.js
@@ -8,6 +8,10 @@ function isArray(value) {
     return Array.isArray(value);
 }
 
+function isArrayOf(value, fn) {
+    return Array.isArray(value) && value.every(fn);
+}
+
 function isObject(value) {
     return typeof value === 'object' && !Array.isArray(value) && value !== null;
 }
@@ -39,13 +43,48 @@ export function validate(body) {
     if (!isObject(body.params))
         return getError(body.id, 'Invalid or missing request parameters value');
 
-    // Validate RPC parameters.
+    // Validate config parameter.
     if (body.params.config) {
         if (!isObject(body.params.config))
-            return getError(body.id, 'Invalid or missing config parameter');
+            return getError(body.id, 'Invalid config parameter');
 
         for (const c of Object.values(body.params.config))
             if (isArray(c) || isObject(c))
                 return getError(body.id, 'Config parameter does not support array values or nested objects');
+    }
+
+    // Validate all other parameters.
+    function validateXdip(params) {
+        if (!isString(params.xdip))
+            return getError(body.id, 'Invalid or missing XDIP parameter');
+    }
+
+    function validateRequestParameters(params) {
+        if (params.requestParameters) {
+            if (!isObject(params.requestParameters))
+                return getError(body.id, 'Invalid request parameters');
+
+            if (params.requestParameters.projectionScopes)
+                if (!isArrayOf(params.requestParameters.projectionScopes, isString))
+                    return getError(body.id, 'Invalid projection scopes parameter');
+
+            if (params.requestParameters.projectionIncludes)
+                if (!isArrayOf(params.requestParameters.projectionIncludes, isString))
+                    return getError(body.id, 'Invalid projection includes parameter');
+
+            if (params.requestParameters.projectionExcludes)
+                if (!isArrayOf(params.requestParameters.projectionExcludes, isString))
+                    return getError(body.id, 'Invalid projection excludes parameter');
+        }
+    }
+
+    switch (body.method) {
+        case Method.ENTITY_GET:
+            return validateXdip(body.params) ?? validateRequestParameters(body.params);
+        case Method.ENTITY_DELETE:
+        case Method.ENTITY_GET_BINARY:
+            return validateXdip(body.params);
+
+        // TODO: Validate all other methods as well.
     }
 }
